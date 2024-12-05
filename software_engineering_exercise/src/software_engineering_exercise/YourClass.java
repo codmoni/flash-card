b54d15e80ed2a40ee18e4b66fec3ff1e69ca841e
doing <doing@DESKTOP-2S5V4B1>
2024-12-05 14:00:13 +0900

    Change in master branch

--- software_engineering_exercise/src/software_engineering_exercise/YourClass.java
+++ software_engineering_exercise/src/software_engineering_exercise/YourClass.java
@@ -3,5 +3,6 @@ package software_engineering_exercise;
 public class YourClass {
 	public void sayHello(String toWhom) {
 		System.out.println("Hello!" +toWhom);
+		System.out.println("Your change in master branch");
 	}
 }
