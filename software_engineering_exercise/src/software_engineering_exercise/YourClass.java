866087da5bc0a0e0dff179b37fd554ae2d7eb220
codmoni <160315926+codmoni@users.noreply.github.com>
2024-12-09 13:36:20 +0900

    Delete software_engineering_exercise directory

--- software_engineering_exercise/src/software_engineering_exercise/YourClass.java
+++
@@ -1,8 +0,0 @@
-package software_engineering_exercise;
-
-public class YourClass {
-	public void sayHello(String toWhom) {
-		System.out.println("Hello!" +toWhom);
-		System.out.println("Your change in master branch");
-	}
-}
